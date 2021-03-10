e7cf7134750a4e4c9210e867e3fb05d48f35625d
Lismey Plasencia <lismeyplasencia@gmail.com>
2021-03-09 20:06:38 -0500

    added routes and updatedb function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// Require express
-const express = require('require');
+// Require express, 
+const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
@@ -12,35 +12,66 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+fs.readFile('db/db.json', 'utf8', (err, data) => {
+    
+if (err) throw err;
 
 // Data
-const note = JSON.parse(data);
+const notes = JSON.parse(data);
 
-// API Routes
+
 // Setup the API/note get route
 
 app.get('/api/notes', (req, res) => {
-    return res.json(note);
-    })
+    return res.json(notes);
+    });
 
-// Setup the API/note post route
-app.post('/api/notes',(req, res) =>{
+// Setup the API/notes post route
+app.post('/api/notes',(req, res) => {
 
     const newNote = req.body;
     console.log(newNote);
-    note.push(newNote);
+    notes.push(newNote);
+    updateDb();
     res.json(newNote);
-})
+});
 
 
+// Gets a note with an specific id
+app.get('/api/notes/:id', (req, res)=>{
+    res.json(notes[req.params.id]);
+});
+
+// Deletes a note with an specific id
+app.delete('/api/notes/:id', (req, res)=>{
+    notes.splice(req.params.id,1);
+    updateDb();
+});
+
+
+// Routes
 
 // Basic route that sends the user to the AJAX page
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+// Display index.html when all other routes are accessed
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', function(req,res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// UpdateDB function
+function updateDb() {
+    fs.writeFile('db/db.json',JSON.stringify(notes,'\t'), err => {
+        if (err) throw err;
+        return true; 
+    })
+}
 
 
+})
 
 
 
